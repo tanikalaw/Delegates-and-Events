898b59636aebda42c10a3181f940047e693bd095
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-09-14 12:54:53 +0800

    - - populate sample data

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+
 using System.Threading.Tasks;
+using DataLibrary;
 
 namespace ConsoleUI
 {
     class Program
     {
+       static ShoppingCartModel cart = new ShoppingCartModel();
+
         static void Main(string[] args)
         {
+            PopulateOrders();
+
+            Console.WriteLine($"The total amount is {cart.TotalItems():C2}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the application...");
+            Console.ReadKey();
+        }
+
+        private static void PopulateOrders()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Blueberry", Price = 19.30M });
+            cart.Items.Add(new ProductModel { ItemName = "Chocolate", Price = 29.70M });
+            cart.Items.Add(new ProductModel { ItemName = "Nuts", Price = 19.30M });
+            cart.Items.Add(new ProductModel { ItemName = "Cheese", Price = 29.70M });
+
         }
     }
 }
